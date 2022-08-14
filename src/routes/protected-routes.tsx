d2d6926cb8a172d1aabe6c792b4fec9084ab27e2
anubhav <anubhavthrilling24@gmail.com>
2022-08-14 14:41:58 +0530

    fix(Commons): Remove un used files

--- src/routes/protected-routes.tsx
+++ src/routes/protected-routes.tsx
@@ -2,29 +2,11 @@ import { lazy } from 'react'
 
 const Logout = lazy(() => import('pages/Logout'))
 const Account = lazy(() => import('pages/Account'))
-const EditListingGeneral = lazy(
-  () => import('pages/EditListing/EditListingGeneral'),
-)
 const Home = lazy(() => import('pages/Home'))
-const EditListingPhotos = lazy(
-  () => import('pages/EditListing/EditListingPhotos'),
-)
-const EditListingPrice = lazy(
-  () => import('pages/EditListing/EditListingPrice'),
-)
-const EditListingEquipments = lazy(
-  () => import('pages/EditListing/EditListingEquipments'),
-)
-const EditTechnicalDetails = lazy(
-  () => import('pages/EditListing/EditTechnicalDetails'),
-)
 const Listings = lazy(() => import('pages/Listings/Listings'))
 const AddListing = lazy(() => import('pages/AddListing'))
-
 const GhatList = lazy(() => import('pages/GhatsList/GhatList'))
-
 const AddGhatForm = lazy(() => import('components/AddGhatForm/AddGhatForm'))
-
 const EditGhat = lazy(() => import('pages/EditGhat/EditGhat'))
 
 export const protectedRoutes = [
@@ -37,36 +19,6 @@ export const protectedRoutes = [
     name: 'account',
     path: '/account',
     component: Account,
-    exact: true,
-  },
-  {
-    name: 'yacht-edit-general',
-    path: '/yacht/:yachtId/general',
-    component: EditListingGeneral,
-    exact: true,
-  },
-  {
-    name: 'yacht-edit-photos',
-    path: '/yacht/:yachtId/photos',
-    component: EditListingPhotos,
-    exact: true,
-  },
-  {
-    name: 'yacht-edit-price',
-    path: '/yacht/:yachtId/price',
-    component: EditListingPrice,
-    exact: true,
-  },
-  {
-    name: 'yacht-edit-equipment',
-    path: '/yacht/:yachtId/equipment',
-    component: EditListingEquipments,
-    exact: true,
-  },
-  {
-    name: 'yacht-edit-description',
-    path: '/yacht/:yachtId/technical',
-    component: EditTechnicalDetails,
     exact: true,
   },
   {
