3113d4ae809b56c339300a4162e72a12f5fa629f
anubhav <anubhavthrilling24@gmail.com>
2022-08-17 21:26:25 +0530

    fix(Footer): Update footer

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -8,10 +8,51 @@ export interface WidgetFooterMenu {
   title: string
   menus: CustomLink[]
 }
+const widgetMenus: WidgetFooterMenu[] = [
+  {
+    id: '1',
+    title: '',
+    menus: [
+      {
+        href: 'https://www.privacypolicygenerator.info/live.php?token=7WkcWeAQ5ooMWqgqYW3NrUlkqgLrxuhT',
+        label: 'Privacy Policy',
+      },
+      { href: '#', label: 'About Us' },
+      { href: '#', label: 'Contact Us' },
+      {
+        href: 'https://www.termsandconditionsgenerator.com/live.php?token=6yNhysk889kMFaQNVUr3DDr7qfimcA40',
+        label: 'Terms & Conditions',
+      },
+      { href: '#', label: 'Cancellation/Refund Policy' },
+    ],
+  },
+]
+const renderWidgetMenuItem = (menu: WidgetFooterMenu, index: number) => {
+  return (
+    <div key={index} className="text-sm">
+      <h2 className="font-semibold text-neutral-700 dark:text-neutral-200">
+        {menu.title}
+      </h2>
+      <ul className="mt-5 grid grid-cols-4 gap-2 mx-5 text-xs">
+        {menu.menus.map((item, index) => (
+          <li key={index}>
+            <a
+              key={index}
+              className="text-neutral-6000 dark:text-neutral-300 hover:text-black dark:hover:text-white"
+              href={item.href}
+            >
+              {item.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 const Footer: React.FC = () => {
   return (
-    <div className="nc-Footer relative py-6 lg:py-32 border-t border-neutral-200 dark:border-neutral-700">
+    <div className="nc-Footer absolute -bottom-80 py-6 lg:py-32 border-t border-neutral-200 dark:border-neutral-700">
       <div className="container grid grid-cols-2 gap-y-10 gap-x-5 sm:gap-x-8 md:grid-cols-4 lg:grid-cols-5 lg:gap-x-10 ">
         <div className="grid grid-cols-4 gap-5 col-span-2 md:col-span-4 lg:md:col-span-1 lg:flex lg:flex-col">
           <div className="col-span-2 md:col-span-1">
@@ -22,6 +63,7 @@ const Footer: React.FC = () => {
           </div>
         </div>
       </div>
+      {widgetMenus.map(renderWidgetMenuItem)}
     </div>
   )
 }
