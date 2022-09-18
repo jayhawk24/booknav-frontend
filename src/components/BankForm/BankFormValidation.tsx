8cfa61abbe2b4873cb906abe4ac00f42758fa761
Ashish Gupta <ashishgupt25@gmail.com>
2022-09-18 18:06:05 +0530

    fix validation

--- src/components/BankForm/BankFormValidation.tsx
+++ src/components/BankForm/BankFormValidation.tsx
@@ -1,8 +1,18 @@
 import * as Yup from 'yup'
 
 export const LoginSchema = Yup.object().shape({
-  accountName: Yup.string().required('Account Name is required'),
-  accountNumber: Yup.string().required('Account Number is required'),
-  bankName: Yup.string().required('Bank Name is required'),
-  ifscCode: Yup.string().required('IFSC is required'),
+  accountName: Yup.string()
+    .min(3, 'Must be more than 2 characters')
+    .required('Account Name is required'),
+  accountNumber: Yup.string()
+    .min(6, 'Enter more than 5 number')
+    .max(24, 'Too Long!')
+    .required('Account Number is required'),
+  bankName: Yup.string()
+    .min(4, 'Must be more than 3 characters')
+    .required('Bank Name is required'),
+  ifscCode: Yup.string()
+    .min(11, 'Required 11 characters')
+    .max(13, 'Too long!')
+    .required('IFSC is required'),
 })
