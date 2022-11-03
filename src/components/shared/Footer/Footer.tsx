168fc65529a910a7bb795e548e83881baa7bfbd6
anubhav <anubhavthrilling24@gmail.com>
2022-11-03 10:59:20 +0530

    add about

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -18,7 +18,7 @@ const widgetMenus: WidgetFooterMenu[] = [
         href: 'https://www.privacypolicygenerator.info/live.php?token=7WkcWeAQ5ooMWqgqYW3NrUlkqgLrxuhT',
         label: 'Privacy Policy',
       },
-      { href: 'booknaav.com/about.html', label: 'About Us' },
+      { href: 'https://booknaav.com/about.html', label: 'About Us' },
       { href: '#', label: 'Contact Us' },
       {
         href: 'https://www.termsandconditionsgenerator.com/live.php?token=6yNhysk889kMFaQNVUr3DDr7qfimcA40',
